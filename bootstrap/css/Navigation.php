c6d11e919c312382a86a100c13632193f0c1f6e9
Mikkel Høst <mikhoest@gmail.com>
2016-10-18 20:42:51 +0200

    Slideshow part 1
    Sstill trying to find out why the images disappear after the animation
    is done.

--- bootstrap/css/Navigation.php
+++ bootstrap/css/Navigation.php
@@ -6,10 +6,9 @@ $menu->add('Home', '');
 
 $about = $menu->add('About', 'about');
 
-// since this item has sub items we append a caret icon to the hyperlink text
 $about->link->append(' <span class="caret"></span>');
 
-// we can attach HTML attributes to the hyper-link as well
+
 $about->link->attributes(['class' => 'link-item', 'target' => '_blank']);
 
 $about->attributes('data-model', 'nice');
@@ -17,11 +16,9 @@ $about->attributes('data-model', 'nice');
 $t = $about->add('Who we are?', array('url' => 'who-we-are',  'class' => 'navbar-item whoweare'));
 $about->add('What we do?', array('url' => 'what-we-do',  'class' => 'navbar-item whatwedo'));
 
-
 $menu->add('Portfolio', 'portfolio');
 $menu->add('Contact',   'contact');
 
-// we're only going to hide items with `display` set to **false**
 
 $menu->filter( function($item){
     if( $item->meta('display') === false) {
