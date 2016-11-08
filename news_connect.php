30b33359e0588d61f9507d742d8a7a36f3974198
Mikkel Høst <mikhoest@gmail.com>
2016-11-08 08:50:07 +0100

    News connection start + Logo suggestion
    Also some other little things

--- news_connect.php
+++ news_connect.php
@@ -1,18 +1,49 @@
 <?php
 
-class ClassParent {
-    public function doThing() {
-        echo "I'm the parrot, squawk!<br>";
-    }
+
+$mysqli = new mysqli("localhost:8080", "root", "", "news");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to Database: " . $mysqli->connect_error;
 }
 
-class ClassChild extends ClassParent {
-    public function doThing()
-    {
-        echo "I'm the babybird, tweetytweet!<br>";
-        parent::doThing();
+$query = $mysqli->query("SELECT * FROM news");
+$row = $query->fetch_assoc();
+echo $row['news'];
+
+$user="root";
+$pass="";
+    $DBH = new PDO('mysql:host=localhost:8080;dbname=news', $user, $pass);
+    foreach ($DBH->query('SELECT * FROM news') as $row) {
+        echo $row[1];
     }
+    $DBH = null;
+
+
+$servername = "localhost:8080";
+$username = "root";
+$password = "123456";
+$db = "news";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("INSERT INTO news (title, description, area) VALUES (:title, :description, :date)");
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':date', $date);
+
+    $title = "Title 01";
+    $description = "The Descriptions 02";
+    $date = "07-11-2016";
+    $stmt->execute();
+
+    echo "Created successfullyferfdjfnsjf";
+
 }
 
-$obj = new ClassChild();
-$obj->doStuff();
+catch (PDOException $e){
+    echo "Error: " . $e->getMessage();
+}
+
+$conn = null;
