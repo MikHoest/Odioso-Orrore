9578e4202c2affc8944a8a4f1f3b5f8bb3706bf3
Mikkel Høst <mikhoest@gmail.com>
2016-11-11 11:00:09 +0100

    loginlink

--- front.php
+++ front.php
@@ -55,7 +55,7 @@ ul
     <li><a href="Menu.php" style="float: none"><img src="picz/MENU-MENU.png" ></a></li>
     <li><a href="theNews.php" style="float: none"><img src="picz/MENU-NEWS.png"></a></li>
     <li><a href="Reservation.php" style="float: none"><img src="picz/MENU-RESERVATIONS.png" ></a></li>
-    <li><a href="login.php" style="float: none"><img src="picz/MENU-LOGIN.png" ></a></li>
+    <li><a href="Login/login.php" style="float: none"><img src="picz/MENU-LOGIN.png" ></a></li>
     <li class="icon">
         <a href="javascript:void(0)" style="font-size:30px;" onclick="myFunction()">
             <p class="customfont"> ☰    ☰ </p>
