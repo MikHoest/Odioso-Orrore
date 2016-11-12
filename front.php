ce040dd5bce43966058651e4d2cae7f72f5e533f
Mikkel Høst <mikhoest@gmail.com>
2016-11-12 14:27:13 +0100

    new slideshow
    not done, but images don't disappear in this one

--- front.php
+++ front.php
@@ -67,7 +67,7 @@ ul
 
 
 
-</div>
+</div class="SlideshowContainer">
 
 <script>
     function myFunction() {
@@ -80,56 +80,53 @@ ul
     }
 </script>
 
-<div class="slideshow-container">
+<style>
+    .mySlides {display:none}
+    .w3-left, .w3-right, .w3-badge {cursor:pointer}
+    .w3-badge {height:13px;width:13px;padding:0}
+</style>
 
-    <div class="mySlides fade">
-        <div class="numbertext">1 / 3</div>
-        <img src="picz/slide2.png" style="width:100%">
-       </div>
+<div class="w3-content w3-display-container">
+    <img class="mySlides" src="picz/slide1.png" style="width:100%">
+    <img class="mySlides" src="picz/slide2.png" style="width:100%">
+    <img class="mySlides" src="picz/slide1.png" style="width:100%">
+    <div class="w3-center w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+        <div class="w3-left w3-padding-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+        <div class="w3-right w3-padding-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+    </div>
+</div>
 
-       <div class="mySlides fade">
-           <div class="numbertext">2 / 3</div>
-           <img src="picz/slide1.png" style="width:100%">
-       </div>
+<script>
+    var slideIndex = 1;
+    showDivs(slideIndex);
 
-       <div class="mySlides fade">
-           <div class="numbertext">3 / 3</div>
-           <img src="picz/slide2.png" style="width:100%">
-       </div>
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
 
-    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    function currentDiv(n) {
+        showDivs(slideIndex = n);
+    }
 
-   </div>
-   <br>
-
-   <div style="text-align:center">
-       <span class="dot" onclick="currentSlide(1)"></span>
-       <span class="dot" onclick="currentSlide(2)"></span>
-       <span class="dot" onclick="currentSlide(3)"></span>
-   </div>
-
-   <script>
-       var slideIndex = 0;
-       showSlides();
-
-       function showSlides() {
-           var i;
-           var slides = document.getElementsByClassName("mySlides");
-           var dots = document.getElementsByClassName("dot");
-           for (i = 0; i < slides.length; i++) {
-               slides[i].style.display = "none";
-           }
-           slideIndex++;
-           if (slideIndex> slides.length) {slideIndex = 1}
-           for (i = 0; i < dots.length; i++) {
-               dots[i].className = dots[i].className.replace(" active", "");
-           }
-           slides[slideIndex-1].style.display = "block";
-           dots[slideIndex-1].className += " active";
-           setTimeout(showSlides, 5000);
-       }
-   </script>
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = x.length}
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" w3-white", "");
+        }
+        x[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " w3-white";
+    }
+</script>
 
 
    <style type="text/css">
