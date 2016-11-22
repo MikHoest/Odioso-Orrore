7a45c2fde865abd9d9d23d09c5909a5f6ece0f1d
FridrikOlafsson <olafsson.fridrik@gmail.com>
2016-11-22 10:14:19 +0100

    fixing some naming conventions DB

--- admin/newMenu.php
+++ admin/newMenu.php
@@ -1,22 +1,20 @@
 <?php
-require_once("../admin/include/session.php");
-require_once("../admin/include/connection.php");
-require_once("../admin/include/functions.php");
+require_once("include/session.php");
+require_once("include/connection.php");
+require_once("include/functions.php");
 confirm_logged_in();
-
-
-
 
 //$db = mysqli_connect('DB_SERVER','DB_USER','DB_PASS','DB_NAME') //C-PANEL DATABASE
 $db = mysqli_connect('DB_SERVER_X','DB_USER_X','DB_PASS_X','DB_NAME_X') //LOCAL DATABASE
 or die('Error connecting to MySQL server.');
 
-$query = "INSERT INTO `news`(`ID`, `title`, `description`, `date`) VALUES ([],[$title],[$description],[$date])";
+$query = "INSERT INTO menuitems(`ID`, `mainCourse`, `ingredients`, `price`, 'review') VALUES ([],[$mainCourse],[$ingredients],[$price], [review])";
 mysqli_query($db, $query) or die('Error querying database.');
 
-$title=$row['title'];
-$description=$row['description'];
-$date=$row['date'];
+$mainCourse=$row['mainCourse'];
+$ingredients=$row['ingredients'];
+$price=$row['price'];
+$review=$row['review'];
 
 ?>
 
@@ -28,19 +26,20 @@ $date=$row['date'];
 
 <body>
 <h1 align="center">Welcome to the NewMenu section</h1>
-<div class="rating-form">
+<div class="wrapper">
 
     <form action="newMenu.php" method="post">
-        <input type="text" style="background-color: #caaea6"  name="name" placeholder="Name" size="30" align="center"><br/>
-        <input type="text" style="background-color: #caaea6" name="email" placeholder="Email" size="30" align="right"><br/>
-        <textarea class="nooResize" name="message" style="background-color: #caaea6" cols="30" placeholder= "Message" rows="5" align="right"></textarea><br/>
+        <input type="text" style="background-color: #ffffff" name="maincourse" placeholder="Main Course" size="30" align="center">.maincourse.<br/>
+   <!-- <input type="text" style="background-color: #ffffff" name="ingredients" placeholder="Ingredients" size="30" align="right"><br/>  -->
+        <textarea class="nooResize" name="ingredients" style="background-color: #caaea6" cols="30" placeholder= "Ingredients" rows="5" align="right">.ingredients.</textarea><br/>
         <style>
             textarea.nooResize
             {
                 resize: none;
             }
         </style>
-        <input type="submit" name="submit" value="SEND!" />
+        <input type="number" style="background-color: #ffffff" name="price" placeholder="Price" size="30" align="left">." DKK.-"<br/>
+        <input type="submit" name="submit" value="Add To Menu!" />
     </form>
 </div>
 </body>
