9dcae7470bd738762a39779dc862315f181a7b7d
Mikkel Høst <mikhoest@gmail.com>
2016-11-28 10:18:55 +0100

    mobile scaling

--- front.php
+++ front.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" type="text/css" href="bootstrap" />
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <style>
 ul
         {
