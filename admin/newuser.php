dd8d253b589dd3c579d42dea0bb08c1afbeac621
FridrikOlafsson <olafsson.fridrik@gmail.com>
2016-12-02 12:40:49 +0100

    added confirm password to create user

--- admin/newuser.php
+++ admin/newuser.php
@@ -19,18 +19,25 @@ if (isset($_POST['submit'])) { // Form has been submitted.
 	// perform validations on the form data
 	$username = trim(mysqli_real_escape_string($connection, $_POST['user']));
 	$password = trim(mysqli_real_escape_string($connection, $_POST['pass']));
+	$confirm_password = trim(mysqli_real_escape_string($connection, $_POST['con_pass']));
     $iterations = ['cost' => 10];//?What is this? = number of times hased!
     $hashed_password = password_hash($password, PASSWORD_BCRYPT, $iterations);
+	$hashed_confirm_password = password_hash($confirm_password, PASSWORD_BCRYPT, $iterations);
 
-	$query = "INSERT INTO login (userName, password) VALUES ('{$username}', '{$hashed_password}')";
+	$query = "INSERT INTO login (userName, password, confirm_password) VALUES ('{$username}', '{$hashed_password}','{$hashed_confirm_password}')";
 
 	$result = mysqli_query($connection, $query);
-		if ($result) {
-			$message = "User Created.";
-			redirect_to("index.php");
+		if($password != $confirm_password)
+		{
+			$message = "Password Don´t match";
 		}
 		else
 		{
+			if ($result)
+			{
+				$message = "User Created.";
+				redirect_to("index.php");
+			}
 			$message = "User could not be created.";
 			$message .= "<br />" . mysqli_error();
 		}
@@ -47,9 +54,11 @@ if (!empty($message))
 <h2>Create New User</h2>
 <form action="newuser.php" method="post">
 Username:
-<input type="text" name="user"  maxlength="30" value="" />
+<input type="text" name="user"  maxlength="30" value="" /><br>
 Password:
-<input type="password" name="pass" maxlength="30" value="" />
+<input type="password" name="pass" maxlength="30" value="" /><br>
+Confirm password:
+<input type="confirm_password" name="con_pass" maxlength="30" value="" /><br>
 <input type="submit" name="submit" value="Create" />
 </form>
 </div>
