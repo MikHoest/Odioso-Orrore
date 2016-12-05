effde3bac6577203531ea9c023785997a36eb410
Mikkel Høst <mikhoest@gmail.com>
2016-12-05 16:33:53 +0100

    removed the weird box

--- slidescript.js
+++ slidescript.js
@@ -9,14 +9,7 @@
     fileref.setAttribute("href", filename);
     document.getElementsByTagName("head")[0].appendChild(fileref);
 
-    var demoad = document.createElement('div');
-    demoad.id = 'cdawrap';
-    demoad.innerHTML = '<span id="cda-remove" class="cda-remove" data-content="Continue to demo" aria-label="Close ad"></span>';
-    document.getElementsByTagName('body')[0].appendChild(demoad);
-
-    document.getElementById('cda-remove').addEventListener('click',function(e){
-        demoad.style.display = 'none';
-        e.preventDefault();
+   
     });
 
     var bsa = document.createElement('script');
