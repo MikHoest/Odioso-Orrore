418f7e6c0a07472756cbfb89d257132f848108cd
FridrikOlafsson <olafsson.fridrik@gmail.com>
2016-12-12 14:59:44 +0100

    reservation thingy

--- Reservation.php
+++ Reservation.php
@@ -5,15 +5,21 @@ require_once("admin/include/functions.php");
 
 if(isset($_POST{'submit'})) {
 
-    $name = $row['name'];
-    $telephone = $row['telephone'];
-    $tableNumber = $row['tableNumber'];
-    $fromTime = $row['fromTime'];
-    $date = $row['date'];
-    $numberGuest = $row['numberGuest'];
-
-    $query = "INSERT INTO reservation( 'name', 'tableNumber', 'fromTime', 'telephone', 'date', 'numberGuest') VALUES ($name, $tableNumber, $fromTime, $telephone, $date, $numberGuest)";
+    $name = trim(htmlspecialchars(mysqli_real_escape_string($connection,$_POST['name'])));
+    $telephone = trim(htmlspecialchars(mysqli_real_escape_string($connection,$_POST['telephone'])));
+    $tableNumber = trim(htmlspecialchars(mysqli_real_escape_string($connection,$_POST['table'])));
+    $timeID = trim(htmlspecialchars(mysqli_real_escape_string($connection,$_POST['timeID'])));
+    $date = trim(htmlspecialchars(mysqli_real_escape_string($connection,$_POST['date'])));
+    //$numberGuest = $_POST['numberGuest'];
+    if($tableNumber < 4)
+    {
+        echo "Please select a valid Table!";
+    }
+    else
+    {
+    $query = "INSERT INTO `reservation`(`name`, `tableID`, `telephone`, `date`, `timeID`) VALUES ('$name', '$tableNumber', '$telephone', '$date', '$timeID')";
     mysqli_query($connection, $query) or die('Error querying database.');
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -80,17 +86,34 @@ if(isset($_POST{'submit'})) {
     </script>
 <li id="home"><a href="front.php" style="float: inherit"><img src="picz/MENU-HOME%20-%20Kopi.png" onmouseover="this.src='picz/MENU-HOME-HOVER'" onmouseout="this.src='picz/MENU-HOME%20-%20Kopi.png'"></a></li>
 <br><br><br>
-<div class="wrapper popup social" style="margin-left: 25%; height: inherit; align-content: center";>
+<div class="wrapper popup social" style="height: inherit; align-content: center";>
     <form action="Reservation.php" method="post">
         <table style=" width: 100%">
-            <p class="customfont" style="font-size: 45px; padding: 20px; font-weight: bold; text-align: center; color: black;">Reserve a Table</p> <!--  $tableNumber, $, $, $ -->
+            <p class="customfont" style="font-size: 45px; padding: 20px; font-weight: bold; text-align: center; color: black;">Reserve a Table</p> <!--  -->
+            <tr><th><select name="table">
+                <option value="1">Table 1</option>
+                <option value="2">Table 2</option>
+                <option value="3">Table 3</option>
+                <option value="4">Table 4</option>
+            </select>
+                </th></tr>
             <tr><th><p class="customfont" style="font-size: 30px; text-align: left; color: black;">Name<th><input type="text" name="name" placeholder="Name" size="30" align="center" required autofocus></th></tr>
             <tr><th><p class="customfont" style="font-size: 30px; text-align: left; color: black;">Phone<th><input type="text" name="telephone" placeholder="Phone number" minlength="8" maxlength="11" required></th></tr>
             <tr><th><p class="customfont" style="font-size: 30px; text-align: left; color: black;">Date<th><input type="text" name="date" id="datepicker" required></p></th></tr>
-            <tr><th><p class="customfont" style="font-size: 30px; text-align: left; color: black;">Time<p class="customfont" style="font-size: 15px; text-align: left; color: black;">no reservation needed between 13:00-17:00 and after 21:00</p><th><input list="browsers" name="fromTime" required><datalist id="browsers"><option value="11:00"><option value="11:30"><option value="12:00"><option value="12:30"><option value="13:00"><option value="17:00"><option value="17:30"><option value="18:00"><option value="18:30"><option value="19:00"><option value="19:30"><option value="20:00"><option value="20:30"><option value="21:00"></option></datalist></p></th></tr>
-            <tr><th><p class="customfont" style="font-size: 30px; text-align: left; color: black;">Number Of Guests<p class="customfont" style="font-size: 15px; text-align: left; color: black;">For groups larger then 8 please call and reserve</p><th><input type="number" name="numberGuest" min="1" max="8" required></p></th></tr>
+            <tr><th><p class="customfont" style="font-size: 30px; text-align: left; color: black;">Time<p class="customfont" style="font-size: 15px; text-align: left; color: black;">no reservation needed before 17:00 and after 21:00</p><th><select name="timeID">
+                        <option value="1700">17:00</option>
+                        <option value="1730">17:30</option>
+                        <option value="1800">18:00</option>
+                        <option value="1830">18:30</option>
+                        <option value="1900">19:00</option>
+                        <option value="1930">19:30</option>
+                        <option value="2000">20:00</option>
+                        <option value="2030">20:30</option>
+                        <option value="2100">21:00</option>
+                    </select></th></tr>
+            <tr><th><p class="customfont" style="font-size: 30px; text-align: left; color: black;">Number Of Guests<p class="customfont" style="font-size: 15px; text-align: left; color: black;">For groups larger then 6 please call and reserve</p><th><input type="number" name="numberGuest" min="1" max="6" required></p></th></tr>
             <!--<tr><th><p class="customfont" style="font-size: 30px; font-weight: bold; text-align: left; color: black;">Select a Table<th><input type="text" name="tableNumber" id="output"><br/></th></tr>-->
-            <tr><th><br><input name="Submit" type="submit" onclick="return validate();" value="Book Your Table"> <input name="Submit" type="submit" value="Check if Available"> <input type="reset"></th></tr>
+            <tr><th><br><input name="submit" type="submit" onclick="return validate();" value="Book Your Table"> <input name="Submit" type="submit" value="Check if Available"> <input type="reset"></th></tr>
         </table>
     </form>
 </div>
@@ -106,6 +129,7 @@ $calendar = new Calendar();
 echo $calendar->show();
 ?>
 </body>-->
+    <div class="container"></div>
 <footer>
     <p class="customfont" style="font-size: 20px; position: relative; text-align: center;">☠ Opening Hours: Monday - Thurday: 10-22 Friday - Saturday: 12-00 Sundays: 12-22<br><br><a href="admin/login.php" style="color: white">© 2016 - Odioso Orrore - ☠</a></p>
 </footer>
