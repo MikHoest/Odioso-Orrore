5ded6a4df911bceac39c6d7cc2eff54e60b8b834
FridrikOlafsson <olafsson.fridrik@gmail.com>
2016-12-13 15:04:03 +0100

    some more stuff -->

--- admin/newMenu.php
+++ admin/newMenu.php
@@ -37,8 +37,7 @@ if(isset($_POST{'publish'})) {
         </style>
         <input type="number" style="background-color: #ffffff" name="price" placeholder="Price" size="30" align="left"> DKK.-<br/>
         <form action="../upload/upload_file.php" method="post" enctype="multipart/form-data">
-            <label for="file">Picture:
-                <input type="file" name="picture"></label>
+            <label for="file">PICTURE --><input type="file" name="picture"></label>
             <!--      <input type="submit" name="submit" value="Upload"> -->
         </form>
         <input type="submit" name="publish" value="Add To Menu!" />
